2f02effb4436f04d82d7bab75d76b192eea780ec
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2023-10-29 11:57:30 +0300

    решение задачи

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ const canWrite = true;
 
 console.log(`Системный файл ${isAdmin && canWrite}`);
 console.log(`Обычный файл ${isAdmin || canWrite}`);
+console.log(`Инвентируем права ${!isAdmin}`);
+const isEdited = true;
+const isSuperAdmin = true;
+console.log(
+  `Файл редактирование ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`
+);
+// задачка
+
+const balace = 1050;
+const isBanned = true;
+const isExist = true;
+const isSelling = true;
+
+if (balace > 1000 && isBanned && isExist && isSelling) {
+  console.log("вы можете купить игру");
+} else {
+  console.log("игра вам недоступна");
+}
