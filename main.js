b04b149f3463f25aeb53c2940505422277e646f9
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2023-10-31 13:28:58 +0300

    функции

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ function countDepositSum(depositUSD, month, rate) {
 }
 const example1 = countDepositSum(1000, 24, 0.12);
 console.log(example1);
+
+function powerOfTwo(num) {
+  return num * num;
+}
+console.log(powerOfTwo(5));
+
+const poft = function (num) {
+  return num * num;
+};
+
+console.log(poft(26));
